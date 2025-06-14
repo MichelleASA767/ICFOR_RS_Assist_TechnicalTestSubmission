076787b8f3ec632d246e3bc89a2f6b03245fcb9b
MichelleASA <agustarannymichelle@gmail.com>
2025-06-14 11:11:27 +0700

    Update GEECode.js

--- GEECode.js
+++ GEECode.js
@@ -2,3 +2,106 @@
 //                      Remote Sensing Assistant Technical Test Submission                             //
 //                            By : Michelle Agustaranny Sekar Arum                                     //
 //=====================================================================================================//
+
+////1.Data Input and Definition, AOI Definition, and Object Centering
+var AOI = SHP_JakartaTimur;
+var referencedata = LocationCrowdAnnotationPerSample;
+Map.addLayer(LocationCrowdAnnotationPerSample);
+Map.addLayer(AOI);
+Map.centerObject(AOI);
+
+////2. Cloud Masking
+function maskL8sr(image) {
+  // Bits 3 and 5 are cloud shadow and cloud, respectively.
+  var cloudShadowBitMask = 1 << 3;
+  var cloudsBitMask = 1 << 5;
+  // Get the pixel QA band.
+  var qa = image.select('QA_PIXEL');
+  // Both flags should be set to zero, indicating clear conditions.
+  var mask = qa.bitwiseAnd(cloudShadowBitMask).eq(0)
+      .and(qa.bitwiseAnd(cloudsBitMask).eq(0));
+  // Return the masked image, scaled to reflectance, without the QA bands.
+  return image.updateMask(mask).divide(10000)
+      .select(['SR_B1','SR_B2','SR_B3','SR_B4','SR_B5','SR_B6','SR_B7'])
+      .copyProperties(image, ["system:time_start"]);
+}
+
+////3. Calling the Landsat 8 Data and Cloud Masking for Data Testing
+var image = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
+              .filterDate('2018-01-01', '2018-12-31')
+              .filterBounds(AOI)
+              .map(maskL8sr)
+              .median()
+              .clip(AOI)
+              print(image, '2018');
+
+Map.addLayer(image, imageVisParam, 'Jakarta Timur Imagery 2018')
+
+////4. Variables
+// Spectral Indices for Landsat 8 Surface Reflectance
+// Bands expected: nir = B5, red = B4, swir1 = B6, swir2 = B7
+//Implemented from source Github https://github.com/HadiEO/RestoreMapping/blob/main/restorelcmap/1_Generate_Composites_Covariates_Functions.js
+
+function NDVI(image) {
+  return image.normalizedDifference(['SR_B5', 'SR_B4']).multiply(10000).int16().rename('ndvi');
+}
+
+function NDWI(image) {
+  return image.normalizedDifference(['SR_B5', 'SR_B6']).multiply(10000).int16().rename('ndwi');
+}
+
+function NBR(image) {
+  return image.normalizedDifference(['SR_B5', 'SR_B7']).multiply(10000).int16().rename('nbr');
+}
+
+function SAVI(image) {
+  var savi = image.expression(
+    '(1 + L) * ((NIR - RED) / (NIR + RED + L))', {
+      'NIR': image.select('SR_B5').toFloat(),
+      'RED': image.select('SR_B4').toFloat(),
+      'L': 0.9
+    });
+  return savi.multiply(10000).int16().rename('savi');
+}
+
+function EVI2(image) {
+  var evi2 = image.expression(
+    '(2.5 * (NIR - RED)) / (NIR + 2.4 * RED + 1)', {
+      'NIR': image.select('SR_B5').toFloat(),
+      'RED': image.select('SR_B4').toFloat()
+    });
+  return evi2.multiply(10000).int16().rename('evi2');
+}
+
+function calcIndicesLandsat(image) {
+  return image
+    .addBands(NDVI(image))
+    .addBands(NDWI(image))
+    .addBands(NBR(image))
+    .addBands(SAVI(image))
+    .addBands(EVI2(image));
+}
+
+var composite = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2')
+              .filterDate('2018-01-01', '2018-12-31')
+              .filterBounds(AOI)
+              .map(maskL8sr)
+              .median()
+              .clip(AOI)
+              print(image, '2018');
+  
+var imageWithIndices = calcIndicesLandsat(composite);
+
+//Indices Visualization Parameter 
+var visNDVI = {min: 0, max: 9000, palette: ['white', 'green']};
+var visNDWI = {min: -1000, max: 8000, palette: ['brown', 'blue']};
+var visNBR  = {min: -1000, max: 8000, palette: ['white', 'black']};
+var visSAVI = {min: 0, max: 8000, palette: ['yellow', 'darkgreen']};
+var visEVI2 = {min: 0, max: 8000, palette: ['pink', 'blue']};
+
+//Visualizing indices(variables) to Map
+Map.addLayer(imageWithIndices.select('ndvi'), visNDVI, 'NDVI');
+Map.addLayer(imageWithIndices.select('ndwi'), visNDWI, 'NDWI');
+Map.addLayer(imageWithIndices.select('nbr'), visNBR, 'NBR');
+Map.addLayer(imageWithIndices.select('savi'), visSAVI, 'SAVI');
+Map.addLayer(imageWithIndices.select('evi2'), visEVI2, 'EVI2');
